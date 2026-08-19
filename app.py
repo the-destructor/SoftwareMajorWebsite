471384fc6c1a8f4c940caa0f1ae4638d0ed46dcf
the-destructor <kane.wilson12@education.nsw.gov.au>
2026-08-19 19:57:08 +1000

    Created the User system
    - Made it so users can log in, log out etc securely
    - Created a database that stores user information encrypted or hashed
    - Made it so you need a CSRF token to do forms
    - Made it so when you log in you're saved to the session so you stay logged-in between pages
    - Made it so the form to log in, sign up and log out work
    - Made it so the current page you are on is highlighted in the navbar
    - Used a previous project to add a bunch of the security stuff

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, url_for, redirect, request, render_template
-
+from flask import Flask, url_for, redirect, request, render_template, abort, session
+import user_management as dbHandler
 import random                               # Generates Random numbers
 import hashlib                              # contains hash function
 from email.message import EmailMessage      # Needed to Creat Email
@@ -8,12 +8,49 @@ import time
 import sqlite3
 from datetime import datetime
 import re
+from cryptography.fernet import Fernet
+import secrets
 
 # Code snippet for logging a message
 # app.logger.critical("message")
 
 app = Flask(__name__)
+app.secret_key = secrets.token_hex(32)
 
+
+def generate_and_save_key():
+    key = Fernet.generate_key()
+    with open("secret.key", "wb") as key_file:
+        key_file.write(key)
+
+def load_key():
+    return open("secret.key", "rb").read()
+
+def generate_csrf_token():
+    if "csrf_token" not in session:
+        session["csrf_token"] = secrets.token_hex(32)
+    return session["csrf_token"]
+app.jinja_env.globals["csrf_token"] = generate_csrf_token
+
+def validate_token():
+    token = session.get("csrf_token")
+    form_token = request.form.get("csrf_token")
+
+    if not token or token != form_token:
+        abort(400)
+
+def encrypt_message(message_text):
+    key = load_key()
+    encoded_message = message_text.encode()
+    f = Fernet(key)
+    encrypted_message = f.encrypt(encoded_message)
+    return encrypted_message
+
+def decrypt_message(encrypted_message_text):
+    key = load_key()
+    f = Fernet(key)
+    decrypted_message = f.decrypt(encrypted_message_text)
+    return decrypted_message.decode()
 
 def is_strong_password(password):
     if len(password) < 8:
@@ -32,11 +69,83 @@ def is_strong_password(password):
 def hash_password(password, salt):
     return hashlib.sha256((password + salt).encode()).hexdigest()
 
+@app.route("/logout", methods=["POST", "GET"])
+def logout():
+    try:
+        if request.method == "GET" and request.args.get("url"):
+                   url = request.args.get("url", "")
+                   return redirect(url, code=302)
+        if request.method == "POST":
+            validate_token()
+            session.clear()
+            return redirect(url_for("home"))
+        else:
+            return render_template("logout.html")
+        
+    except:
+        print("error in logout")
+        return render_template("logout.html")
+    
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    try:
+        if session.get("logged_in"):
+            print("User is logged in:", session["username"])
+            return redirect(url_for("logout"))
+        if request.method == "GET" and request.args.get("url"):
+            url = request.args.get("url", "")
+            return redirect(url, code=302)
+        if request.method == "POST":
+            validate_token()
+            username = request.form["username"]
+            password = request.form["password"]
+            hashed_password = hash_password(password, dbHandler.retrieveSalt(username))
+            isLoggedIn = dbHandler.retrieveUsers(username, hashed_password)
+            session["username"] = username
+            session["logged_in"] = isLoggedIn
+            if isLoggedIn:
+                return redirect(url_for("home"))
+            else:
+                return render_template("login.html")
+        else:
+            return render_template("login.html")
+    except:
+        print("error in login")
+        return render_template("login.html")
+
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    try:
+        if session.get("logged_in"):
+            print("User is logged in:", session["username"])
+        if request.method == "GET" and request.args.get("url"):
+            url = request.args.get("url", "")
+            return redirect(url, code=302)
+        if request.method == "POST":
+            validate_token()
+            username = request.form["username"]
+            password = request.form["password"]
+            if not is_strong_password(password):
+                return render_template("signup.html")
+            new_email = encrypt_message(request.form["email"])
+            new_salt = secrets.token_hex(16)
+            hashed_password = hash_password(password, new_salt)
+            dbHandler.insertUser(username, hashed_password, new_salt, new_email)
+            return redirect("/")
+        else:
+            return render_template("signup.html")
+    except:
+        print("error in signup")
+        return render_template("signup.html")
 
 @app.route("/index.html", methods=["POST", "GET"])
 @app.route("/", methods=["POST", "GET"])
 def home():
     try:
+        if session.get("logged_in"):
+            print("User is logged in:", session["username"])
         if request.method == "GET" and request.args.get("url"):
             url = request.args.get("url", "")
             return redirect(url, code=302)
---
+++ user_management.py
@@ -0,0 +1,96 @@
+import sqlite3 as sql
+import time
+import random                            # contains hash function
+from email.message import EmailMessage      # Needed to Creat Email                              # make password hidden                              # Used to create a timeout for the validation code
+import secrets
+from datetime import datetime
+import re                             # Needed to send email
+import hashlib                              # contains hash function
+import getpass                              # make password hidden                               # Used to create a timeout for the validation code
+from cryptography.fernet import Fernet      # pip install cryptography
+
+def load_key():
+    return open("secret.key", "rb").read()
+
+def encrypt_message(message_text):
+    key = load_key()
+    encoded_message = message_text.encode()
+    f = Fernet(key)
+    encrypted_message = f.encrypt(encoded_message)
+    return encrypted_message
+
+def decrypt_message(encrypted_message_text):
+    key = load_key()
+    f = Fernet(key)
+    decrypted_message = f.decrypt(encrypted_message_text)
+    return decrypted_message.decode()
+
+
+def is_valid_email_regex(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+$'
+    # basically its allowing the characters a-z, A-Z, 0-9, ., _, %, + and - in the first one, the plus means its one or more characters, 
+    # the @ means it needs an @ and then it allows the characters a-z, A-Z, 0-9, . and -
+    # then it needs a dot and then some characters that are atleast 2 characters long
+    if re.fullmatch(pattern, email):
+        return True
+    else:
+        return False
+
+def insertUser(username, password, salt, email):
+    con = sql.connect("database_files/database.db")
+    cur = con.cursor()
+    cur.execute(
+        "INSERT INTO users (username,password,salt,email) VALUES (?,?,?,?)",
+        (username, password, salt, email),
+    )
+    con.commit()
+    con.close()
+
+def retrieveUsers(username, password):
+    con = sql.connect("database_files/database.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE username = ?",(username,))
+    if cur.fetchone() == None:
+        con.close()
+        return False
+    else:
+        cur.execute("SELECT * FROM users WHERE username = ? AND password = ?",(username, password,))
+        if cur.fetchone() == None:
+            con.close()
+            return False
+        else:
+            con.close()
+            return True
+        
+def retrieveSalt(username):
+    con = sql.connect("database_files/database.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM users WHERE username = ?",(username,))
+    if cur.fetchone() == None:
+        con.close()
+        return False
+    else:
+        cur.execute("SELECT salt FROM users WHERE username = ?",(username,))
+        return cur.fetchone()[0]
+
+
+def insertFeedback(feedback):
+    con = sql.connect("database_files/database.db")
+    cur = con.cursor()
+    cur.execute("INSERT INTO feedback (feedback) VALUES (?)",(feedback,))
+    con.commit()
+    con.close()
+
+
+def listFeedback():
+    con = sql.connect("database_files/database.db")
+    cur = con.cursor()
+    data = cur.execute("SELECT * FROM feedback").fetchall()
+    con.close()
+    f = open("templates/partials/success_feedback.txt", "w")
+    for row in data:
+        f.write(f"{row[1]}\n")
+    f.close()
+    with open("templates/partials/success_feedback.txt") as f:
+        feedback = [line.strip() for line in f]
+    return feedback
